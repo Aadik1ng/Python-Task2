95e55e5bbb8849ca58e83977a34155584fad744e
Aaditya <aadityaaryan639@gmail.com>
2025-01-23 21:33:16 +0530

    Added Phone removed Age

--- library_CRUD/routes/users.py
+++ library_CRUD/routes/users.py
@@ -5,9 +5,9 @@ from app.users import add_user, list_users, edit_user, remove_user
 router = APIRouter()
 
 @router.post("/")
-def create_user(name: str, email: str, age: int):
+def create_user(name: str, email: str, Phone: int):
 
-    return add_user(name, email, age)
+    return add_user(name, email, Phone)
 
 @router.get("/")
 def get_users():
@@ -15,9 +15,9 @@ def get_users():
     return list_users()
 
 @router.put("/{user_id}")
-def update_user(user_id: int, name: str = None, email: str = None, age: int = None):
+def update_user(user_id: int, name: str = None, email: str = None, Phone: int = None):
 
-    return edit_user(user_id, name, email, age)
+    return edit_user(user_id, name, email, Phone)
 
 @router.delete("/{user_id}")
 def delete_user(user_id: int):
