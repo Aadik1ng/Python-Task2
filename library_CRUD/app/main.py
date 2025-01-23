e884232de43f23a7e07897b3bf479afd9e0ae4f5
Aaditya <aadityaaryan639@gmail.com>
2025-01-24 00:22:38 +0530

    Include borrow router

--- library_CRUD/app/main.py
+++ library_CRUD/app/main.py
@@ -3,6 +3,7 @@ from fastapi import FastAPI
 from app.database import create_tables,check_and_create_db
 from routes.books import router as book_router
 from routes.users import router as user_router
+from routes.borrow import router as borrow_router
 
 
 app = FastAPI()
@@ -11,6 +12,7 @@ create_tables()
 
 app.include_router(book_router, prefix="/books", tags=["Books"])
 app.include_router(user_router, prefix="/users", tags=["Users"])
+app.include_router(borrow_router, prefix="/borrow", tags=["Borrow"])
 
 
 @app.get("/")
